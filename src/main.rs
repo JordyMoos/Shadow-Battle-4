d5f9baf819970acc5958ab55de64573ac14dcfd3
Jordy Moos <jordymoos@gmail.com>
2018-06-10 19:18:51 +0200

    Playing with actors

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
-extern crate actix;
-use actix::{msgs, Actor, Addr, Arbiter, Context, Syn, System};
+#[macro_use] extern crate actix;
+use std::time::Duration;
+use actix::prelude::*;
 
-
-struct MyActor;
-
-impl Actor for MyActor {
-    type Context = Context<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        println!("I am alive!");
-        Arbiter::system().do_send(msgs::SystemExit(0));
-    }
+#[derive(Message)]
+struct Ping {
+    pub id: usize,
 }
 
+struct Game {
+    counter: usize,
+    addr: Recipient<Unsync, Ping>,
+}
+
+impl Actor for Game {
+    type Context = Context<Game>;
+}
+
+impl Handler<Ping> for Game {
+    type Result = ();
+
+    fn handle(&mut self, msg: Ping, ctx: &mut Context<Self>) {
+        self.counter += 1;
+
+        if self.counter > 10 {
+            Arbiter::system().do_send(actix::msgs::SystemExit(0));
+        } else {
+            println!("Ping received {:?}", msg.id);
+
+            ctx.run_later(Duration::new(1, 0), move |act, _| {
+                act.addr.do_send(Ping { id: msg.id + 1 });
+            });
+        }
+    }
+}
 
 fn main() {
     let system = System::new("test");
 
-    let addr: Addr<Syn, _> = MyActor.start();
+    let _: Addr<Unsync, _> = Game::create(|ctx| {
+        let addr: Addr<Unsync, _> = ctx.address();
+        let addr2: Addr<Unsync, _> = Game {
+            counter: 0,
+            addr: addr.recipient()
+        }.start();
+
+        addr2.do_send(Ping { id: 10 });
+
+        Game {
+            counter: 0,
+            addr: addr2.recipient(),
+        }
+    });
 
     system.run();
 }
