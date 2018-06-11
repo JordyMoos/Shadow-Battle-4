07b94a7687f49a09ea6339c2bac0ece371489629
Jordy Moos <jordymoos@gmail.com>
2018-06-11 23:18:30 +0200

    Setting up the web server

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,24 @@
-#[macro_use] extern crate actix;
-use std::time::Duration;
-use actix::prelude::*;
+extern crate actix_web;
 
-#[derive(Message)]
-struct Ping {
-    pub id: usize,
+use actix_web::{http, App, HttpRequest, server};
+use std::cell::Cell;
+
+struct AppState {
+    counter: Cell<usize>,
 }
 
-struct Game {
-    counter: usize,
-    addr: Recipient<Unsync, Ping>,
-}
+fn index(req: HttpRequest<AppState>) -> String {
+    let count = req.state().counter.get() + 1;
+    req.state().counter.set(count);
 
-impl Actor for Game {
-    type Context = Context<Game>;
-}
-
-impl Handler<Ping> for Game {
-    type Result = ();
-
-    fn handle(&mut self, msg: Ping, ctx: &mut Context<Self>) {
-        self.counter += 1;
-
-        if self.counter > 10 {
-            Arbiter::system().do_send(actix::msgs::SystemExit(0));
-        } else {
-            println!("Ping received {:?}", msg.id);
-
-            ctx.run_later(Duration::new(0, 100_000_000), move |act, _| {
-                act.addr.do_send(Ping { id: msg.id + 1 });
-            });
-        }
-    }
+    format!("Request number: {}", count)
 }
 
 fn main() {
-    let system = System::new("test");
-
-    let _: Addr<Unsync, _> = Game::create(|ctx| {
-        let addr: Addr<Unsync, _> = ctx.address();
-        let addr2: Addr<Unsync, _> = Game {
-            counter: 0,
-            addr: addr.recipient()
-        }.start();
-
-        addr2.do_send(Ping { id: 10 });
-
-        Game {
-            counter: 0,
-            addr: addr2.recipient(),
-        }
-    });
-
-    system.run();
+    server::new(|| {
+        App::with_state(AppState { counter: Cell::new(0) })
+            .resource("/", |r| r.method(http::Method::GET).f(index))
+    }).bind("127.0.0.1:8080")
+        .unwrap()
+        .run();
 }
